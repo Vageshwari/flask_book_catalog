5faee10cddf208864d23999117b5d1fae2746a22
suresh <dd_suresh@hotmail.com>
2019-02-12 10:21:20 +0530

    few changes

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from app import create_app, db
 from app.auth.models import User
 from sqlalchemy import exc
 
-flask_app = create_app('prod')
+"""
+#This is for creating all database objects using dev confiuration
+flask_app = create_app('dev')
 with flask_app.app_context():
     db.create_all()
     try:
@@ -10,5 +12,10 @@ with flask_app.app_context():
             User.create_user(user='harry', email='harry@potters.com', password='secret')
     except exc.IntegrityError:
             flask_app.run()
+"""            
+#This is for running site using dev confiuration
+if (__name__ == '__main__'):
+    flask_app = create_app('dev')
+    flask_app.run()
 
 
